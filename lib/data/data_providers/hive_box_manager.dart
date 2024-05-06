0647df859f07832da3ed51a61a9f25daad14cf6e
Urban Potato <veryprogramming@gmail.com>
2024-05-06 19:53:07 +0700

    Remove Debug messages

--- lib/data/data_providers/hive_box_manager.dart
+++ lib/data/data_providers/hive_box_manager.dart
@@ -1,6 +1,4 @@
-import 'package:get_it/get_it.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:talker_flutter/talker_flutter.dart';
 import 'package:to_do_list_app/data/entities/task.dart';
 
 class HiveBoxManager {
@@ -18,8 +16,7 @@ class HiveBoxManager {
     if (Hive.isBoxOpen(boxName)) {
       final counter = _boxCounter[boxName] ?? 1;
       _boxCounter[boxName] = counter + 1;
-      GetIt.I<Talker>()
-          .info('(1 open (was open) - $boxName) _boxCounter = $_boxCounter');
+
       return Hive.box<Task>(boxName);
     }
 
@@ -27,27 +24,21 @@ class HiveBoxManager {
     if (!Hive.isAdapterRegistered(taskAdapterTypeId)) {
       Hive.registerAdapter(TaskAdapter());
     }
-    GetIt.I<Talker>().info(
-        '(2 open (was closed, we open) - $boxName) _boxCounter = $_boxCounter');
+
     return Hive.openBox<Task>(boxName);
   }
 
   Future<void> closeTaskBox(String boxName) async {
     if (!Hive.isBoxOpen(boxName)) {
       _boxCounter.remove(boxName);
-      GetIt.I<Talker>()
-          .info('(1 close (was closed) - $boxName) _boxCounter = $_boxCounter');
+
       return;
     }
 
     var counter = _boxCounter[boxName] ?? 1;
     counter -= 1;
     _boxCounter[boxName] = counter;
-    if (counter > 0) {
-      GetIt.I<Talker>().info(
-          '(2 close (was open, remain open, reduce counter) - $boxName) _boxCounter = $_boxCounter');
-      return;
-    }
+    if (counter > 0) return;
 
     _boxCounter.remove(boxName);
 
@@ -55,8 +46,6 @@ class HiveBoxManager {
 
     await box.compact();
     await box.close();
-    GetIt.I<Talker>().info(
-        '(3 close (was open, we close) - $boxName) _boxCounter = $_boxCounter');
   }
 }
 
